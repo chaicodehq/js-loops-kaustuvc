15642a62eb351c2af191d3878044d479ee5938d1
kaustuvc <kaustuvchatterjee1234@gmail.com>
2026-03-02 17:07:40 +0530

    chore: solve 07-ipl-points-table.js

--- src/07-ipl-points-table.js
+++ src/07-ipl-points-table.js
@@ -37,5 +37,71 @@
  *   // Sorted: CSK(3), RCB(1), MI(0)
  */
 export function iplPointsTable(matches) {
-  // Your code here
+  const accArr = []
+  if (!Array.isArray(matches) || matches.length === 0)
+    return accArr
+  
+  for (let i = 0; i < matches.length; i++){
+    let match = matches[i]
+    let team1Index = accArr.findIndex(teamDetails => teamDetails.team ===  match.team1)
+    let team2Index = accArr.findIndex(teamDetails => teamDetails.team === match.team2)
+    if (team1Index === -1) {
+      const acc = {
+        team: match.team1,
+        played: 0,
+        won: 0,
+        lost: 0,
+        tied: 0,
+        noResult: 0,
+        points: 0
+      }
+      accArr.push(acc)
+      team1Index = accArr.findIndex(teamDetails => teamDetails.team === match.team1)
+    }
+    if (team2Index === -1) {
+      const acc = {
+        team: match.team2,
+        played: 0,
+        won: 0,
+        lost: 0,
+        tied: 0,
+        noResult: 0,
+        points: 0
+      }
+      accArr.push(acc)
+      team2Index = accArr.findIndex(teamDetails => teamDetails.team === match.team2)
+    }
+    
+    accArr[team1Index].played += 1
+    accArr[team2Index].played += 1
+    
+    switch (match.result.toLowerCase()) {
+      case "win":
+        if (match.team1 === match.winner) {
+          accArr[team1Index].won += 1
+          accArr[team1Index].points += 2
+          accArr[team2Index].lost += 1
+        } else {
+          accArr[team2Index].won += 1
+          accArr[team2Index].points += 2
+          accArr[team1Index].lost += 1
+        }
+        break
+      case "tie":
+        accArr[team1Index].points += 1
+        accArr[team2Index].points += 1
+        accArr[team1Index].tied += 1
+        accArr[team2Index].tied += 1
+        break
+      case "no_result":
+        accArr[team1Index].points += 1
+        accArr[team1Index].noResult += 1
+        accArr[team2Index].points += 1
+        accArr[team2Index].noResult += 1
+        break
+      default:
+        break
+    }
+  }
+  return accArr.toSorted((a, b) => b.points - a.points || a.team.localeCompare(b.team))
 }
